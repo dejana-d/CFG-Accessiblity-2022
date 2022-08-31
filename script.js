ab24fbf28bc366bf662547281e05555258ec82bd
arpinehjaniyan <81558239+arpinehjaniyan@users.noreply.github.com>
2022-08-31 13:36:40 +0100

    Modified the tablinks

--- script.js
+++ script.js
@@ -120,3 +120,18 @@ function myFunctionWomen() {
 }
 
 
+
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active1", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active1";
+}
