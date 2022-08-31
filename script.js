77a92889e45587c98ffe8b5c2cdea6809021483b
arpinehjaniyan <81558239+arpinehjaniyan@users.noreply.github.com>
2022-08-31 15:35:15 +0100

    Created slideshows

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("homepageImage");
-  var dots = document.getElementsByClassName("numberButton");
+  var dots = document.getElementsByClassName("dot");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -28,8 +28,6 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-black";
 }
-
-
 
 
 
